c2a9c77673766d1bd2629d2594112df8739fe1c3
solof <solofonore@gmail.com>
2024-02-06 15:18:42 +0300

    test jenkinsfile

---
+++ script.groovy
@@ -0,0 +1,13 @@
+def buildApp(){
+    echo 'building the application'
+}
+
+def testApp(){
+    echo 'testing the application'
+}
+
+def deployApp(){
+    echo 'deploy on production'
+}
+
+return this
